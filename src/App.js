0287f84411bd747f7561c13a9587ab70edd5bf9c
Gopal Adhikari <gadhikari@covermymeds.com>
2018-01-04 14:49:31 -0500

    Add packages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div className='ui container'>
+      <Route path="/" exact component={HomePage} />
+    </div>);
   }
 }
 
