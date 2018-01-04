262c815df8d64ecbbebf2c96a413aa78a8015d7d
Gopal Adhikari <gadhikari@covermymeds.com>
2018-01-04 14:49:48 -0500

    Add API gateway

---
+++ src/api/index.js
@@ -0,0 +1,36 @@
+const API_URL = 'http://api.football-data.org/v1';
+const TOKEN = '78e9523b969e468e867b16260a01cf69';
+
+function headers() {
+  return {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'X-Auth-Token': TOKEN
+  };
+}
+
+function parseResponse(response) {
+  return response.json().then((json) => {
+    if (!response.ok) {
+      return Promise.reject(json);
+    }
+    return json;
+  });
+}
+
+function queryString(params) {
+  const query = Object.keys(params)
+                      .map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`)
+                      .join('&');
+  return `${query.length ? '?' : ''}${query}`;
+}
+
+export default {
+  fetch(url, params = {}) {
+    return fetch(`${API_URL}${url}${queryString(params)}`, {
+      method: 'GET',
+      headers: headers(),
+    })
+    .then(parseResponse);
+  }
+}
