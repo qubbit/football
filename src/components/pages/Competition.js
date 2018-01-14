7e837626013eff3d2f32b47f9f0fd60b8c8b10ff
Gopal Adhikari <gadhikari@covermymeds.com>
2018-01-13 23:34:08 -0500

    Whatever

--- src/components/pages/Competition.js
+++ src/components/pages/Competition.js
@@ -1,66 +1,60 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { fetchTeams, fetchCompetition } from "../../actions";
-import Fixtures from "./Competition/Fixtures.js";
-import Standings from "./Competition/Standings.js";
-import { Link } from "react-router-dom";
-import League from "../ui/League";
-import { Loader, Button } from "semantic-ui-react";
-import PropTypes from "prop-types";
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchTeams, fetchCompetition} from '../../actions';
+import Fixtures from './Competition/Fixtures.js';
+import Standings from './Competition/Standings.js';
+import {
+  withRouter,
+  Route,
+  Link,
+} from 'react-router-dom';
+import League from '../ui/League';
+import {Loader, Button} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
 class Competition extends Component {
   componentDidMount() {
-    const { match: { params } } = this.props;
-    // Promise.all([this.props.fetchCompetition(params.id), this.props.fetchTeams(params.id)]);
+    const {match: {params}} = this.props;
     this.props
       .fetchCompetition(params.id)
       .then(this.props.fetchTeams(params.id));
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    // debugger;
+    return (this.props.match.params.id !== nextProps.match.params.id)
+    // console.log('shouldComponentUpdate');
+  }
+
   render() {
-    const { competition, loading } = this.props;
-    const page = this.props.match.params.page;
+    const {competition, loading} = this.props;
 
     if (loading) {
-      // return <Loader size='large'>Loading...</Loader>
+      // return null;
+      return <Loader size="large">Loading...</Loader>;
     }
-
-    let renderPage = null; // <Fixtures competition={competition} />
-
-    switch (page) {
-      case "fixtures":
-        renderPage = <Fixtures competition={competition} />;
-        break;
-      case "standings":
-        renderPage = <Standings competition={competition} />;
-        break;
-      case "teams":
-        renderPage = <Fixtures competition={competition} />;
-        break;
-      default:
-        renderPage = <Fixtures competition={competition} />;
-        break;
-    }
-
+    const url = this.props.match.url;
     return (
       <div>
         <League key={`league-${competition.id}`} league={competition} />
         <Button.Group size="large" widths="3">
           <Button>
-            <Link to={`/competitions/${competition.id}/fixtures`}>
-              Fixtures
-            </Link>
+            <Link to={`${url}/fixtures`}>Fixtures</Link>
           </Button>
           <Button>
-            <Link to={`/competitions/${competition.id}/standings`}>
-              Standings
-            </Link>
+            <Link to={`${url}/standings`}>Standings</Link>
           </Button>
           <Button>
-            <Link to={`/competitions/${competition.id}/teams`}>Teams</Link>
+            <Link to={`${url}/teams`}>Teams</Link>
           </Button>
         </Button.Group>
-        {renderPage}
+        <div>
+          <Fixtures competition={competition} />
+          <Route path="/competition/:id/fixtures" component={Fixtures} />
+          <Route path="fixtures" component={Fixtures} />
+          <Route path="standings" exact component={Standings} />
+          <Route path={`${url}/teams`} exact component={Fixtures} />
+        </div>
       </div>
     );
   }
@@ -71,17 +65,17 @@ Competition.propTypes = {
   competition: PropTypes.object.isRequired,
   teams: PropTypes.array.isRequired,
   fetchTeams: PropTypes.func.isRequired,
-  fetchCompetition: PropTypes.func.isRequired
+  fetchCompetition: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     loading: state.competitions.loading,
     competition: state.competitions.currentCompetition,
-    teams: state.teams.teams
+    teams: state.teams.teams,
   };
 }
 
-export default connect(mapStateToProps, { fetchTeams, fetchCompetition })(
-  Competition
+export default withRouter(
+  connect(mapStateToProps, {fetchTeams, fetchCompetition})(Competition),
 );
