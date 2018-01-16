8d1ab080958ef4a27b51c5b5feb1315889b7bf0a
Gopal Adhikari <gadhikari@covermymeds.com>
2018-01-16 16:28:23 -0500

    Take out styles from the Teams component
    Styles now live in Teams.scss for this component

--- src/components/pages/Competition/Teams.js
+++ src/components/pages/Competition/Teams.js
@@ -14,78 +14,35 @@ class Teams extends Component {
     const rows = teams.map(t => {
       const code = t.code !== null ? t.code : t.name;
 
-      return <li
-        className="card"
-        style={{
-          textAlign: 'center',
-          width: '25%',
-          padding: '12px 8px',
-          flexGrow: 0,
-          flexShrink: 0,
-          flexBasis: 'auto',
-        }}
-        key={`team-${code}`}>
-        <div
-          className="card-team"
-          style={{
-            backgroundColor: 'rgba(51, 51, 51, 0.06)',
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100%',
-            position: 'relative',
-          }}>
-          <div className="card-logo">
-            <div
-              style={{
-                marginLeft: '20px',
-                marginRight: '20px',
-                marginTop: '20px',
-                paddingTop: '30px',
-                width: 'calc(100% - 40px)',
-              }}
-              className="team-crest-container">
-              <img
-                alt={`Crest of ${t.name}`}
-                style={{height: '150px'}}
-                src={t.crestUrl}
-              />
+      return (
+        <li className="card" key={`team-${code}`}>
+          <div className="card-team">
+            <div className="card-logo">
+              <div className="team-crest-container">
+                <img
+                  className="team-crest"
+                  alt={`Crest of ${t.name}`}
+                  src={t.crestUrl}
+                />
+              </div>
             </div>
+            <div className="card-team-body">
+              <h2>{t.name}</h2>
+            </div>
+            <a
+              className="team-roster-link animated-underline"
+              href="/teams/roster">
+              View Roster
+            </a>
           </div>
-          <div
-            className="card-team-body"
-            style={{textTransform: 'uppercase', flexGrow: 1, padding: '20px'}}>
-            <h2>{t.name}</h2>
-          </div>
-          <a
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              borderTop: '1px solid rgba(51,51,51,.2)',
-              padding: '1.075em 1.5em .925em',
-              textTransform: 'uppercase',
-            }}
-            className="animated-underline"
-            href="/teams/roster">
-            View Roster
-          </a>
-        </div>
-      </li>
+        </li>
+      );
     });
 
     return (
       <section style={{color: '#333'}}>
         <h1>Teams</h1>
-        <ul
-          style={{
-            padding: 0,
-            listStyleType: 'none',
-            display: 'flex',
-            flexWrap: 'wrap',
-            flexDirection: 'row',
-            justifyContent: 'flex-start',
-          }}>
-          {rows}
-        </ul>
+        <ul className="competition-team-list">{rows}</ul>
       </section>
     );
   }
