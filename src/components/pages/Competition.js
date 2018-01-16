0c0b6c83063896e4c32967f2510c8cd3cbaf5e50
Gopal Adhikari <gadhikari@covermymeds.com>
2018-01-15 22:02:55 -0500

    Fuck react-router and everything it represents

--- src/components/pages/Competition.js
+++ src/components/pages/Competition.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import {fetchTeams, fetchCompetition} from '../../actions';
 import Fixtures from './Competition/Fixtures';
 import Standings from './Competition/Standings';
-// import Teams from './Competition/Teams';
+import Teams from './Competition/Teams';
 import League from '../ui/League';
 
 class Competition extends Component {
@@ -15,6 +15,7 @@ class Competition extends Component {
     this.props
       .fetchCompetition(params.id)
       .then(this.props.fetchTeams(params.id));
+      // console.log(this.props); // eslint-disable
   }
 
   render() {
@@ -23,29 +24,26 @@ class Competition extends Component {
     if (loading) {
       return <Loader size="large">Loading...</Loader>;
     }
-    const {match: {url}} = this.props;
     return (
       <div>
         <League key={`league-${competition.id}`} league={competition} />
         <Button.Group size="large" widths="3">
           <Button>
-            <Link to={`${url}/fixtures`}>Fixtures</Link>
+            <Link to={`/competitions/${competition.id}/fixtures`}>Fixtures</Link>
           </Button>
           <Button>
-            <Link to={`${url}/standings`}>Standings</Link>
+            <Link to={`/competitions/${competition.id}/standings`}>Standings</Link>
           </Button>
           <Button>
-            <Link to={`${url}/teams`}>Teams</Link>
+            <Link to={`/competitions/${competition.id}/teams`}>Teams</Link>
           </Button>
         </Button.Group>
         <div>
-          <Fixtures competition={competition} />
           { /* <Standings competitions={competition} /> */ }
           { /* <Teams competitions={competition} /> */ }
-          <Route path="/competition/:id/fixtures" component={Fixtures} />
-          <Route path="fixtures" component={Fixtures} />
-          <Route path="standings" exact component={Standings} />
-          <Route path={`${url}/teams`} exact component={Fixtures} />
+          <Route path="/fixtures" exact component={Fixtures} />
+          <Route path="/competitions/:id/standings" exact component={Standings} />
+          <Route path="/competitions/:id/teams" exact  component={Teams} />
         </div>
       </div>
     );
