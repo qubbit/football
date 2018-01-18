be099d31744b1797c1e95c22a8f3a1a896d5310b
qubbit <gopala@email.arizona.edu>
2018-01-18 17:44:05 -0500

    Do not animate underline on active menu items

--- src/components/ui/MenuBar.js
+++ src/components/ui/MenuBar.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {Input, Dropdown, Icon, Menu} from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Input, Dropdown, Icon, Menu } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 // import {fetchCompetitions} from '../../actions';
 
 class MenuBar extends Component {
   handleSeasonChange = (_, data) => {
-    this.props.fetchCompetitions({season: data.value});
+    this.props.fetchCompetitions({ season: data.value });
   };
 
   render() {
-    const {competition, activeMenuItem, currentSeason} = this.props;
+    const { competition, activeMenuItem, currentSeason } = this.props;
     const years = Array(10)
       .fill()
       .map((_, i) => currentSeason.year - i);
@@ -19,31 +19,30 @@ class MenuBar extends Component {
     const options = years.map(y => ({
       key: `year-${y}`,
       text: `${y}/${y + 1}`,
-      value: y,
+      value: y
     }));
 
     return (
-      <Menu secondary>
-        <Menu.Item name="fixtures" active={activeMenuItem === 'fixtures'}>
-          <Link
-            className="animated-underline"
-            to={`/competitions/${competition.id}/fixtures`}>
-            Fixtures
-          </Link>
+      <Menu secondary className="competition-menu-bar">
+        <Menu.Item
+          className="animated-underline"
+          name="fixtures"
+          active={activeMenuItem === 'fixtures'}>
+          <Link to={`/competitions/${competition.id}/fixtures`}>Fixtures</Link>
         </Menu.Item>
-        <Menu.Item name="standings" active={activeMenuItem === 'standings'}>
-          <Link
-            className="animated-underline"
-            to={`/competitions/${competition.id}/standings`}>
+        <Menu.Item
+          className="animated-underline"
+          name="standings"
+          active={activeMenuItem === 'standings'}>
+          <Link to={`/competitions/${competition.id}/standings`}>
             Standings
           </Link>
         </Menu.Item>
-        <Menu.Item name="teams" active={activeMenuItem === 'teams'}>
-          <Link
-            className="animated-underline"
-            to={`/competitions/${competition.id}/teams`}>
-            Teams
-          </Link>
+        <Menu.Item
+          className="animated-underline"
+          name="teams"
+          active={activeMenuItem === 'teams'}>
+          <Link to={`/competitions/${competition.id}/teams`}>Teams</Link>
         </Menu.Item>
         <Menu.Menu position="right">
           <Menu.Item>
@@ -61,14 +60,14 @@ class MenuBar extends Component {
 }
 
 MenuBar.propTypes = {
-  currentSeason: PropTypes.shape({season: PropTypes.number}).isRequired,
+  currentSeason: PropTypes.shape({ season: PropTypes.number }).isRequired
 };
 
 export default connect(
   state => ({
     currentSeason: state.competitions.currentSeason,
     competition: state.competition.competition,
-    activeMenuItem: state.application.activeMenuItem,
+    activeMenuItem: state.application.activeMenuItem
   }),
-  {},
+  {}
 )(MenuBar);
