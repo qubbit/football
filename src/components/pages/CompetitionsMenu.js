3235e0a8ce8f53fa9e4d99946b4e16c896725c91
qubbit <gopala@email.arizona.edu>
2018-01-19 01:25:30 -0500

    Need to iron out some bugs but competition selection highlight works

--- src/components/pages/CompetitionsMenu.js
+++ src/components/pages/CompetitionsMenu.js
@@ -5,7 +5,12 @@ import {Loader} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import {fetchCompetitions} from '../../actions';
 
-class Competitions extends Component {
+class CompetitionsMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeItem: props.competition.id || 445 }
+  }
+
   componentDidMount() {
     this.props.fetchCompetitions({season: this.props.currentSeason.season});
   }
@@ -14,25 +19,35 @@ class Competitions extends Component {
     this.props.fetchCompetitions({season: data.value});
   };
 
+  handleClick = (e, v) => {
+    this.setState({ activeItem: parseInt(e.target.id, 10) });
+  }
+
   render() {
     const {competitions, loading} = this.props;
+    const { activeItem } = this.state;
 
     if (loading) {
       return <Loader size="large">Loading...</Loader>;
     }
 
+    const items = competitions.map(c => {
+      const activeClass = activeItem === c.id ? ' active' : '';
+      return <Link
+        id={`${c.id}`}
+        className={`competition-link${activeClass}`}
+        key={`competition-${c.id}`}
+        onClick={this.handleClick}
+        to={`/competitions/${c.id}`}>
+        {c.caption}
+      </Link>;
+    })
+
     return (
       <div className="competition-menu-container">
         <h2>Competitions</h2>
         <div className="competition-menu">
-          {competitions.map(c => (
-            <Link
-              className="competition-link"
-              key={`competition-${c.id}`}
-              to={`/competitions/${c.id}`}>
-              {c.caption}
-            </Link>
-          ))}
+          {items}
         </div>
         <footer>
           <p>
@@ -44,7 +59,7 @@ class Competitions extends Component {
   }
 }
 
-Competitions.propTypes = {
+CompetitionsMenu.propTypes = {
   loading: PropTypes.bool.isRequired,
   competitions: PropTypes.arrayOf(PropTypes.object).isRequired,
   currentSeason: PropTypes.shape({season: PropTypes.number}).isRequired,
@@ -54,8 +69,9 @@ Competitions.propTypes = {
 export default connect(
   state => ({
     competitions: state.competitions.competitions,
+    competition: state.competition.competition,
     currentSeason: state.competitions.currentSeason,
     loading: state.competitions.loading,
   }),
   {fetchCompetitions},
-)(Competitions);
+)(CompetitionsMenu);
