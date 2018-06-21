e6f4cb6ef2f28a40f3479926bf61bb31e8c994e1
qubbit <gopala@email.arizona.edu>
2018-06-21 12:07:56 -0400

    Remove asset mapping

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,9 +1,8 @@
 import { NAVIGATE_PAGE } from '../actions/types';
-import * as ASSETS from '../asset_mapping.json';
 
 const initialState = {
   activeMenuItem: 'standings',
-  normalizers: {...ASSETS }
+  normalizers: {}
 };
 
 export default function(state = initialState, action) {
